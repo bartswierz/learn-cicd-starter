dce3e0f86297fe6fbbc7139e8ea61b06d988b217
bartswierz <bartoszswierzynski7@gmail.com>
2024-11-12 23:40:30 -0600

    updated ReadHeaderTimeout to a static 10 seconds

--- main.go
+++ main.go
@@ -96,7 +96,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
-		ReadHeaderTimeout: 10 * time.Second,
+		ReadHeaderTimeout: 10,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
